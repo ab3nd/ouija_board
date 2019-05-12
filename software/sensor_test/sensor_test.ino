7bdc35fb2ebc6c5dd3d4d7d6ecb92ecb42c37ddd
ams <orphrey@gmail.com>
2019-05-12 19:33:12 -0400

    moving testing to real hardware

--- software/sensor_test/sensor_test.ino
+++ software/sensor_test/sensor_test.ino
@@ -1,10 +1,19 @@
 #include <math.h>
 
-#define ENABLE 2
-#define S0 3
-#define S1 4
-#define S2 5
+#define ENABLE_SENSE 4
+#define S0 7
+#define S1 11
+#define S2 12
 #define SENSE A0
+
+#define LIMIT_X 9
+#define LIMIT_Y 10
+
+#define ENABLE_MOTOR 8
+#define DIR_X 5
+#define DIR_Y 6
+#define STEP_X 2
+#define STEP_Y 3
 
 /* Lookup table for the sensors' component calculations. Since the 
  * angles of all the sensors are fixed, this table was precomputed. 
@@ -19,14 +28,56 @@ int cal_readings[8] = {0, 0, 0, 0, 0, 0, 0, 0};
  * so that the readings are sequential around the ring */
 int addr_map[8] = {0, 1, 2, 7, 3, 6, 4, 5};
 
+void move_home(){
+  //Enable stepper drivers
+  digitalWrite(ENABLE_MOTOR, LOW);
+
+  //Home x axis first
+  digitalWrite(DIR_X, LOW);
+  digitalWrite(STEP_X, LOW);
+  while(digitalRead(LIMIT_X) == 0) {
+    digitalWrite(STEP_X,HIGH); 
+    delayMicroseconds(500); 
+    digitalWrite(STEP_X,LOW); 
+    delayMicroseconds(500); 
+  }
+
+  //Home Y axis, it uses opposite DIR_Y setting
+  digitalWrite(DIR_X, LOW);
+  digitalWrite(STEP_Y, LOW);
+  while(digitalRead(LIMIT_Y) == 0){
+    digitalWrite(STEP_Y,HIGH); 
+    delayMicroseconds(500); 
+    digitalWrite(STEP_Y,LOW); 
+    delayMicroseconds(500); 
+  }
+
+  //Enable stepper drivers
+  digitalWrite(ENABLE_MOTOR, HIGH);
+}
+
 void setup() {
-  pinMode(ENABLE, OUTPUT);
+
+  //Limit switches
+  pinMode(LIMIT_X, INPUT);
+  pinMode(LIMIT_Y, INPUT);
+
+  //Set up the stepper control
+  pinMode(ENABLE_MOTOR, OUTPUT);
+  pinMode(DIR_X, OUTPUT);
+  pinMode(DIR_Y, OUTPUT);
+  pinMode(STEP_X, OUTPUT);
+  pinMode(STEP_Y, OUTPUT);
+
+  // Hall ring sensor
+  pinMode(ENABLE_SENSE, OUTPUT);
   pinMode(S0, OUTPUT);
   pinMode(S1, OUTPUT);
   pinMode(S2, OUTPUT);
   pinMode(SENSE, INPUT);
 
-  digitalWrite(ENABLE, HIGH);
+  // Set up sensor
+  digitalWrite(ENABLE_SENSE, HIGH);
   digitalWrite(S0, LOW);
   digitalWrite(S1, LOW);
   digitalWrite(S2, LOW);
@@ -36,12 +87,12 @@ void setup() {
     for(byte ii = 0; ii < 8; ii++)
     {
       //Disable the switch
-      digitalWrite(ENABLE, HIGH);
+      digitalWrite(ENABLE_SENSE, HIGH);
       //Set the address bits
       (ii & B00000001)? digitalWrite(S0, HIGH): digitalWrite(S0, LOW);
       (ii & B00000010)? digitalWrite(S1, HIGH): digitalWrite(S1, LOW);
       (ii & B00000100)? digitalWrite(S2, HIGH): digitalWrite(S2, LOW);
-      digitalWrite(ENABLE, LOW);
+      digitalWrite(ENABLE_SENSE, LOW);
       cal_readings[addr_map[ii]] += analogRead(SENSE);
     }
   }
@@ -54,7 +105,10 @@ void setup() {
     //dividing by 10
     cal_readings[idx] = cal_readings[idx]/10;
   }
-  
+
+  //Home the axes
+  move_home();
+    
   //For debugging
   Serial.begin(9600);
 }
@@ -63,12 +117,12 @@ void loop() {
   for(byte ii = 0; ii < 8; ii++)
   {
     //Disable the switch
-    digitalWrite(ENABLE, HIGH);
+    digitalWrite(ENABLE_SENSE, HIGH);
     //Set the address bits
     (ii & B00000001)? digitalWrite(S0, HIGH): digitalWrite(S0, LOW);
     (ii & B00000010)? digitalWrite(S1, HIGH): digitalWrite(S1, LOW);
     (ii & B00000100)? digitalWrite(S2, HIGH): digitalWrite(S2, LOW);
-    digitalWrite(ENABLE, LOW);
+    digitalWrite(ENABLE_SENSE, LOW);
     readings[addr_map[ii]] = analogRead(SENSE)-cal_readings[addr_map[ii]];
   }
 
@@ -94,14 +148,65 @@ void loop() {
     sumY += float(readings[ii]) * sin_lut[ii];
   }
 
-//  Serial.print(sumX);
-//  Serial.print(",");
-//  Serial.println(sumY);
-
   //Might not need polar conversion, since the system is in X and Y
   Serial.print(sumX);
   Serial.print(",");
   Serial.println(sumY);
+
+  //Set the motor directions (These may need reversing)
+  sumX > 0 ? digitalWrite(DIR_X, HIGH): digitalWrite(DIR_X, LOW);
+  sumY > 0 ? digitalWrite(DIR_Y, HIGH): digitalWrite(DIR_Y, LOW);
+  
+//  //Do some steps
+//  if(sumX > 10 || sumY > 10){
+//    // The change is big enough that we should react to it
+//    int ratio = 0;
+//    if(sumX > sumY){
+//      // Do one step in the Y direction and the ratio steps in X
+//      ratio = int(abs(sumX/sumY));
+//      Serial.print("X larger: ");
+//      Serial.println(ratio);
+//
+//      // Y step
+//      if(digitalRead(LIMIT_Y) == 0){
+//        digitalWrite(STEP_Y,HIGH); 
+//        delayMicroseconds(500); 
+//        digitalWrite(STEP_Y,LOW); 
+//        delayMicroseconds(500); 
+//      }
+//
+//      // X steps
+//      while(ratio > 0 && (digitalRead(LIMIT_X) == 0)){
+//        ratio--;
+//        digitalWrite(STEP_X,HIGH); 
+//        delayMicroseconds(500); 
+//        digitalWrite(STEP_X,LOW); 
+//        delayMicroseconds(500);                 
+//      }
+//    }
+//    else{
+//      // Do one step in the X direction and the ratio steps in Y
+//      ratio = int(abs(sumY/sumX));
+//      Serial.print("Y larger: ");
+//      Serial.println(ratio);
+//      
+//      // Single X step
+//      if(digitalRead(LIMIT_X) == 0){
+//        digitalWrite(STEP_X,HIGH); 
+//        delayMicroseconds(500); 
+//        digitalWrite(STEP_X,LOW); 
+//        delayMicroseconds(500); 
+//      }
+//      // Y step for ratio
+//      while(ratio > 0 && (digitalRead(LIMIT_Y) == 0)){
+//        ratio--;
+//        digitalWrite(STEP_Y,HIGH); 
+//        delayMicroseconds(500); 
+//        digitalWrite(STEP_Y,LOW); 
+//        delayMicroseconds(500);                 
+//      }
+//    }
+//  }
 
 //  //Convert to polar
 //  float r = sqrt(pow(sumX, 2) + pow(sumY, 2));
@@ -109,5 +214,7 @@ void loop() {
 //  //Serial.print(r);
 //  //Serial.print(",");
 //  Serial.println(theta);
+
+  
   
 }
